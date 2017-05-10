3ec0b6c6753e577b5314abc6c1bb99575cbc67e3
Steven Maude <git@stevenmaude.co.uk>
2017-05-10 14:00:21 +0100

    Add some notes

--- diet_gtfs.py
+++ diet_gtfs.py
@@ -1,6 +1,16 @@
 import csv
 import sys
 
+# agency.txt done
+# feed_info.txt nothing to change
+
+# calendar_dates.txt depends on service_id.
+# routes.txt depends on agency.txt
+# shapes.txt depends on trips.txt
+# stops.txt depends on stop_times.txt
+# stop_times.txt depends on trip_id.
+# transfers.txt depends on stop_id from and to, routes.
+# trips.txt contains shape_id, also route_id to trip_id.
 
 def clean_agency_file(*agencies):
     with open('agency.txt', 'r') as f:
