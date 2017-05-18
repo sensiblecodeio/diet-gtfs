42fcfb8acc61092890d489f8a024de01d47ad768
Steven Maude <git@stevenmaude.co.uk>
2017-05-18 17:04:04 +0100

    Read files in from zip file directly
    As that's how they're provided, and it saves unzipping separately.

--- diet_gtfs.py
+++ diet_gtfs.py
@@ -2,8 +2,10 @@
 from collections import Counter
 from decimal import Decimal
 import csv
+import io
 import os
 import sys
+import zipfile
 
 
 def process_trips(trip_ids):
@@ -11,17 +13,20 @@ def process_trips(trip_ids):
     shape_ids = set()
     service_ids = set()
 
-    with open('trips.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('trips.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
 
-        for row in reader:
-            if row[2] in trip_ids:
-                filtered_rows.append(row)
-                route_ids.add(row[0])
-                shape_ids.add(row[9])
-                service_ids.add(row[1])
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
+
+            for row in reader:
+                if row[2] in trip_ids:
+                    filtered_rows.append(row)
+                    route_ids.add(row[0])
+                    shape_ids.add(row[9])
+                    service_ids.add(row[1])
 
     with open('cleaned/trips.txt', 'w') as f:
         writer = csv.writer(f)
@@ -31,14 +36,16 @@ def process_trips(trip_ids):
 
 
 def clean_shapes(shape_ids):
-    with open('shapes.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('shapes.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if row[0] in shape_ids:
-                filtered_rows.append(row)
+            for row in reader:
+                if row[0] in shape_ids:
+                    filtered_rows.append(row)
 
     with open('cleaned/shapes.txt', 'w') as f:
         writer = csv.writer(f)
@@ -50,25 +57,27 @@ def process_stop_times(stop_ids):
 
     trip_id_count = Counter()
 
-    with open('stop_times.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('stop_times.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if row[2] in stop_ids:
-                filtered_rows.append(row)
-                trip_id_count[row[0]] += 1
+            for row in reader:
+                if row[2] in stop_ids:
+                    filtered_rows.append(row)
+                    trip_id_count[row[0]] += 1
 
-        for trip_id, count in trip_id_count.items():
-            if count >= 2:
-                trip_ids.add(trip_id)
+            for trip_id, count in trip_id_count.items():
+                if count >= 2:
+                    trip_ids.add(trip_id)
 
-        cleaned_rows = [filtered_rows[0]]
+            cleaned_rows = [filtered_rows[0]]
 
-        for row in filtered_rows[1:]:
-            if row[0] in trip_ids:
-                cleaned_rows.append(row)
+            for row in filtered_rows[1:]:
+                if row[0] in trip_ids:
+                    cleaned_rows.append(row)
 
     with open('cleaned/stop_times.txt', 'w') as f:
         writer = csv.writer(f)
@@ -80,18 +89,20 @@ def process_stop_times(stop_ids):
 def process_stops(min_lat, max_lat, min_lon, max_lon):
     stop_ids = set()
 
-    with open('stops.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('stops.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            stop_lat = Decimal(row[3])
-            stop_lon = Decimal(row[4])
-            if (stop_lat >= min_lat and stop_lat <= max_lat
-                    and stop_lon >= min_lon and stop_lon <= max_lon):
-                filtered_rows.append(row)
-                stop_ids.add(row[0])
+            for row in reader:
+                stop_lat = Decimal(row[3])
+                stop_lon = Decimal(row[4])
+                if (stop_lat >= min_lat and stop_lat <= max_lat
+                        and stop_lon >= min_lon and stop_lon <= max_lon):
+                    filtered_rows.append(row)
+                    stop_ids.add(row[0])
 
     with open('cleaned/stops.txt', 'w') as f:
         writer = csv.writer(f)
@@ -103,15 +114,17 @@ def process_stops(min_lat, max_lat, min_lon, max_lon):
 def process_routes(route_ids):
     agency_ids = set()
 
-    with open('routes.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('routes.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if row[0] in route_ids:
-                filtered_rows.append(row)
-                agency_ids.add(row[1])
+            for row in reader:
+                if row[0] in route_ids:
+                    filtered_rows.append(row)
+                    agency_ids.add(row[1])
 
     with open('cleaned/routes.txt', 'w') as f:
         writer = csv.writer(f)
@@ -121,15 +134,17 @@ def process_routes(route_ids):
 
 
 def clean_transfers(stop_ids, trip_ids):
-    with open('transfers.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('transfers.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if (row[0] in stop_ids and row[1] in stop_ids
-                and row[4] in trip_ids and row[5] in trip_ids):
-                filtered_rows.append(row)
+            for row in reader:
+                if (row[0] in stop_ids and row[1] in stop_ids
+                    and row[4] in trip_ids and row[5] in trip_ids):
+                    filtered_rows.append(row)
 
     with open('cleaned/transfers.txt', 'w') as f:
         writer = csv.writer(f)
@@ -137,14 +152,16 @@ def clean_transfers(stop_ids, trip_ids):
 
 
 def clean_calendar(service_ids):
-    with open('calendar_dates.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('calendar_dates.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if row[0] in service_ids:
-                filtered_rows.append(row)
+            for row in reader:
+                if row[0] in service_ids:
+                    filtered_rows.append(row)
 
     with open('cleaned/calendar_dates.txt', 'w') as f:
         writer = csv.writer(f)
@@ -152,14 +169,16 @@ def clean_calendar(service_ids):
 
 
 def clean_agencies(agency_ids):
-    with open('agency.txt', 'r') as f:
-        reader = csv.reader(f)
-        filtered_rows = []
-        filtered_rows.append(next(reader))
+    with zipfile.ZipFile('gtfs-nl.zip', 'r') as zipped:
+        with zipped.open('agency.txt', 'r') as zipped_file:
+            f = io.TextIOWrapper(zipped_file)
+            reader = csv.reader(f)
+            filtered_rows = []
+            filtered_rows.append(next(reader))
 
-        for row in reader:
-            if row[0] in agency_ids:
-                filtered_rows.append(row)
+            for row in reader:
+                if row[0] in agency_ids:
+                    filtered_rows.append(row)
 
     with open('cleaned/agency.txt', 'w') as f:
         writer = csv.writer(f)
